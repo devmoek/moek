1c7d0849515a1ddddd350a5a7274822f32c619ef
moek <moekdeveloper@gmail.com>
2022-01-20 17:37:40 +0600

    add all files

---
+++ components/header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <div class="logo">
+            <img src="#" alt="logo">
+        </div>
+        <ul>
+            <a href="index.php"><li>Home</li></a>
+            <a href="about.php"><li>About</li></a>
+            <a href="contact.php"><li>Contact</li></a>
+            <a href="blog.php"><li>Blog</li></a>
+        </ul>
+    </nav>
+</header>
